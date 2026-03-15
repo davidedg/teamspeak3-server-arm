c0897f79834a2a91a912fc8a99244a66e9950a6c
Davide Del Grande <delgrande.davide@gmail.com>
2026-03-15 17:45:13 +0100

    Improve internet connectivity check timeout

--- root/teamspeak/sh/update.sh
+++ root/teamspeak/sh/update.sh
@@ -32,7 +32,7 @@ create_version_file(){
 
 
 #Check internet connectivity
-if !(curl -s --head --fail https://teamspeak.com > /dev/null)
+if !(curl --connect-timeout 1 -s --head --fail https://teamspeak.com > /dev/null)
 then
     echo "Internet connectivity check failed! No update was done."
     return
